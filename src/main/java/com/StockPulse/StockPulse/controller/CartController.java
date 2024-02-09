e3c921061e5b19f4b55ed0a59196dbb6a37a04b1
Luis A Castaneda <lcastaa.aql@gmail.com>
2024-02-09 13:50:04 -0800

    Reconfigued project layout and Incorporated Unit testing functionality

--- src/main/java/com/StockPulse/StockPulse/controller/CartController.java
+++ src/main/java/com/StockPulse/StockPulse/controller/CartController.java
@@ -1,1 +1,6 @@
-// hello - Luis
+package com.StockPulse.StockPulse.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartController{}
---
+++ src/main/java/com/StockPulse/StockPulse/dao/UserDao.java
@@ -0,0 +1,26 @@
+package com.StockPulse.StockPulse.dao;
+
+import com.StockPulse.StockPulse.models.LoginUserDTO;
+import com.StockPulse.StockPulse.models.RegisterUserDTO;
+import com.StockPulse.StockPulse.models.User;
+
+// Will contain the CRUD methods to implement in the JDBC Layer
+public interface UserDao {
+
+
+    // TODO - Create User
+    void RegisterUser(RegisterUserDTO dto);
+
+
+    // TODO - Update User
+    void editUser(User user);
+
+
+    // TODO - Read/Get User
+    User getUserForLogin(LoginUserDTO dto);
+
+
+    // TODO - Delete User
+    void deleteUser(int userId);
+
+}
