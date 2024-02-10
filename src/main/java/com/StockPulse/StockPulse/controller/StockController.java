93784e74b4fc5fdb6395b23407c73695c843b359
David Nguyen <da791195@gmail.com>
2024-02-09 23:16:08 -0600

    More updates to StockController class

--- src/main/java/com/StockPulse/StockPulse/controller/StockController.java
+++ src/main/java/com/StockPulse/StockPulse/controller/StockController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import com.StockPulse.StockPulse.models.Stock;
 
 public class StockController {
-    // This class is used to make updates to a stock object
+    // This class will be updated later.
 
     @Autowired
     private Stock stock;
@@ -15,12 +15,12 @@ public class StockController {
         // This method will be implemented later.
     }
 
-    @PostMapping("")
+    @GetMapping("")
     public void getCost() {
         // This method will be implemented later.
     }
 
-    @PostMapping("")
+    @GetMapping("")
     public void getOwner() {
         // This method will be implemented later.
     }
